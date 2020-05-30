a522d5a8a6520275d306f160dca0f3646334fad2
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-05-30 17:13:22 +0200

    The Doors

---
+++ Assets/ActionOpenDoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionOpenDoor : Action
+{
+    [SerializeField]
+    private bool _IsOpen;
+    bool IsOpen
+    {
+        get { return _IsOpen; }
+        set
+        {
+            _IsOpen = value;
+            UpdateView();
+        }
+    }
+    [SerializeField] TextSetter TextHint;
+    [SerializeField] GameObject DoorsOpen;
+    [SerializeField] GameObject DoorsClosed;
+
+    void Start()
+    {
+        UpdateView();
+    }
+
+    private void UpdateView()
+    {
+        if (IsOpen)
+        {
+            TextHint.SetText("Close door");
+            DoorsOpen.SetActive(true);
+            DoorsClosed.SetActive(false);
+        }
+        else
+        {
+            TextHint.SetText("Open door");
+            DoorsOpen.SetActive(false);
+            DoorsClosed.SetActive(true);
+        }
+    }
+
+    public override void PerformAction()
+    {
+        IsOpen = !IsOpen;
+    }
+}
