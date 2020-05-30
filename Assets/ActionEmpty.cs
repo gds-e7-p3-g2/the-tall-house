a0d35e138f20278f9ecf8a9ee0b6a656c84c5666
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-05-30 16:50:01 +0200

    Walking around the whole house

---
+++ Assets/ActionEmpty.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionEmpty : Action
+{
+    private PlayerController playerController;
+    [SerializeField] TextSetter TextHint;
+    private int TimesUsed = 0;
+
+    void Start()
+    {
+        TextHint.SetText("Action performed times: " + TimesUsed);
+        playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+
+    public override void PerformAction()
+    {
+        TimesUsed++;
+        TextHint.SetText("Action performed times: " + TimesUsed);
+    }
+}
---
+++ Assets/Scripts/Items/ActionTeleport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionTeleport : Action
+{
+    private GameObject Player;
+    [SerializeField] GameObject Destination;
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    public override void PerformAction()
+    {
+        Player.transform.position = new Vector3(Destination.transform.position.x, Destination.transform.position.y, Player.transform.position.z);
+    }
+}
