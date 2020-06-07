0afdaf6a75aa9fc93db26012ac7c471d4bd45018
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-06-08 01:22:56 +0200

    Ghost can see you

---
+++ Assets/GhostSight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class GhostSight : MonoBehaviour
+{
+
+    [SerializeField] Light2D[] lighs;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if (player.GetIsHiding())
+        {
+            return;
+        }
+        foreach (Light2D light in lighs)
+        {
+            light.color = Color.red;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        foreach (Light2D light in lighs)
+        {
+            light.color = Color.blue;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+        if (player.GetIsHiding())
+        {
+            return;
+        }
+        foreach (Light2D light in lighs)
+        {
+            light.color = Color.red;
+        }
+    }
+}
