cf5e5607b66d7f70a24e974a7b658251481baec0
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-07-18 21:16:57 +0200

    Recordable Item

---
+++ Assets/RecordableItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RecordableItem : MonoBehaviour
+{
+    public UnityEvent EnteredFrame;
+    public UnityEvent ExitedFrame;
+    public UnityEvent Recorded;
+    public void BeRecorder()
+    {
+        if (IsInFrame)
+        {
+            Recorded.Invoke();
+        }
+    }
+
+    private bool IsInFrame = false;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("CameraFrame"))
+        {
+            EnteredFrame.Invoke();
+            IsInFrame = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("CameraFrame"))
+        {
+            ExitedFrame.Invoke();
+            IsInFrame = false;
+        }
+    }
+}
