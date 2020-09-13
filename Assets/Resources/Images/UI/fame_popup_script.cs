4e39abb065ae10f9a61805ebef2c5a587a9032f5
estrzel <e.strzelecka5@gmail.com>
2020-09-13 18:54:15 +0200

    add popup, but code doesnt compile
    during qube-player collision, image should appear, but there are some strange compile errors...

---
+++ Assets/Resources/Images/UI/fame_popup_script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class fame_popup_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [SerializeField] private Image customImage;
+    
+    void onTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+        }
+    }
+    
+    void onTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            customImage.enabled = false;
+        }
+    }
+
+  
+}
