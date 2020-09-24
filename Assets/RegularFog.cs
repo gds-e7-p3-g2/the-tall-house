96d3651423e3f8d0e6bb64f7cdb66a1fcf50d966
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-09-24 13:54:25 +0200

    fog of war - first version

---
+++ Assets/RegularFog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IStreamYouScream
+{
+    public class RegularFog : MonoBehaviour
+    {
+        public GameObject Piece;
+        public GameObject LeftBottom;
+        public GameObject RightTop;
+        void Start()
+        {
+            Vector3 position = LeftBottom.transform.position;
+            Vector3 max = RightTop.transform.position;
+
+            GameObject piece = Instantiate(Piece, position, Quaternion.identity, transform);
+            Vector3 size = piece.GetComponent<Renderer>().bounds.size;
+            Destroy(piece);
+
+            float x = position.x;
+            while (x < max.x)
+            {
+                float y = position.y;
+                while (y < max.y)
+                {
+                    Instantiate(Piece, new Vector3(x, y, 0), Quaternion.identity, transform);
+                    y += size.y * .65f;
+                }
+                x += size.x * .65f;
+            }
+        }
+    }
+}
---
+++ Assets/RegularFogPiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace IStreamYouScream
+{
+    public class RegularFogPiece : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            StartCoroutine(FadeImage());
+        }
+
+        private void onEnd()
+        {
+            Destroy(gameObject);
+        }
+
+        IEnumerator FadeImage()
+        {
+            while (gameObject.GetComponent<SpriteRenderer>().color.a > 0)
+            {
+                gameObject.GetComponent<SpriteRenderer>().color -= new Color(0, 0, 0, Time.deltaTime);
+                yield return null;
+            }
+            onEnd();
+        }
+    }
+}
