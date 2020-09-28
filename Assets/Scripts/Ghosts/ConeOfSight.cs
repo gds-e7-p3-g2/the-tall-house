b00611230d9bf3481b2597aacd7c90c857913f5a
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-09-28 03:56:33 +0200

    WIP

--- Assets/Scripts/Ghosts/ConeOfSight.cs
+++ Assets/Scripts/Ghosts/ConeOfSight.cs
@@ -11,7 +11,13 @@ namespace IStreamYouScream
         public Vector3Event OnSeen;
         private void OnTriggerStay2D(Collider2D other)
         {
-            OnSeen.Invoke(other.gameObject.transform.position);
+            // OnSeen.Invoke(other.gameObject.transform.position);
+            gameObject.GetComponent<Animator>().SetBool("Loading", true);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Loading", false);
         }
     }
 
