7d49db46b569a09148a21537ba2e4550d8797b60
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-09-30 21:00:38 +0200

    VVievers fixes

--- Assets/Scripts/ViewersController.cs
+++ Assets/Scripts/ViewersController.cs
@@ -62,77 +62,93 @@ namespace IStreamYouScream
             satisfaction += EventAttractiveness;
         }
     }
+    // public class ViewersController : MonoBehaviour
+    // {
+    //     public List<Viewer> Viewers = new List<Viewer>();
+    //     private int viewersToAdd = 0;
+    //     public int CurrentNumberOfViewers { get { return Viewers.Count; } }
+    //     [SerializeField] int InitialViewers = 5;
+    //     [SerializeField] int InitialSatisfaction = 50;
+    //     [SerializeField] int NumberOfViewerFriends = 1;
+    //     [SerializeField] int InvitationCooldown = 10;
+    //     [SerializeField] IntEvent OnViewersCountChanged;
+
+    //     void Start()
+    //     {
+    //         viewersToAdd = InitialViewers;
+    //         AddViewers();
+    //     }
+    //     public void ReactToEvent(float EventAttractiveness)
+    //     {
+    //         Viewers.ForEach(viewer => viewer.ReactToEvent(EventAttractiveness));
+    //         Viewers.FindAll(viewer => viewer.satisfaction <= 0).ForEach(RemoveViewer);
+    //         AddViewers();
+    //     }
+    //     private void AddViewers()
+    //     {
+    //         for (int i = 0; i < viewersToAdd; i++)
+    //         {
+    //             AddViewer();
+    //         }
+
+    //         viewersToAdd = 0;
+    //     }
+
+    //     private Viewer BuildViewer()
+    //     {
+    //         Viewer viewer = new Viewer(InitialSatisfaction, NumberOfViewerFriends);
+
+    //         viewer.OnInvite.AddListener(OnInvite);
+    //         viewer.OnLeave.AddListener(RemoveViewer);
+
+    //         return viewer;
+    //     }
+
+    //     private void OnInvite(Viewer viewer)
+    //     {
+    //         viewersToAdd++;
+    //         StartCoroutine(Cooldown(viewer));
+    //     }
+
+    //     private IEnumerator Cooldown(Viewer viewer)
+    //     {
+    //         yield return new WaitForSeconds(InvitationCooldown);
+    //         viewer.canInvite = true;
+    //     }
+
+    //     private void AddViewer()
+    //     {
+    //         Viewers.Add(BuildViewer());
+    //         OnViewersCountChanged.Invoke(CurrentNumberOfViewers);
+    //     }
+
+    //     private void RemoveViewer(Viewer viewer)
+    //     {
+    //         viewer.OnInvite.RemoveAllListeners();
+    //         viewer.OnLeave.RemoveAllListeners();
+    //         Viewers.Remove(viewer);
+    //         OnViewersCountChanged.Invoke(CurrentNumberOfViewers);
+
+    //         if (CurrentNumberOfViewers <= 0)
+    //         {
+    //             StoryEvents.Instance.OnLostAllViewers.Invoke();
+    //         }
+    //     }
+    // }
+
     public class ViewersController : MonoBehaviour
     {
-        public List<Viewer> Viewers = new List<Viewer>();
-        private int viewersToAdd = 0;
-        public int CurrentNumberOfViewers { get { return Viewers.Count; } }
         [SerializeField] int InitialViewers = 5;
-        [SerializeField] int InitialSatisfaction = 50;
-        [SerializeField] int NumberOfViewerFriends = 1;
-        [SerializeField] int InvitationCooldown = 10;
+        private int numOfViewers;
+        [SerializeField] int InitialPopularity = 50;
         [SerializeField] IntEvent OnViewersCountChanged;
 
-        void Start()
-        {
-            viewersToAdd = InitialViewers;
-            AddViewers();
-        }
         public void ReactToEvent(float EventAttractiveness)
         {
-            Viewers.ForEach(viewer => viewer.ReactToEvent(EventAttractiveness));
-            Viewers.FindAll(viewer => viewer.satisfaction <= 0).ForEach(RemoveViewer);
-            AddViewers();
-        }
-        private void AddViewers()
-        {
-            for (int i = 0; i < viewersToAdd; i++)
-            {
-                AddViewer();
-            }
-
-            viewersToAdd = 0;
+            numOfViewers += (int)EventAttractiveness;
+            OnViewersCountChanged.Invoke(numOfViewers);
         }
 
-        private Viewer BuildViewer()
-        {
-            Viewer viewer = new Viewer(InitialSatisfaction, NumberOfViewerFriends);
 
-            viewer.OnInvite.AddListener(OnInvite);
-            viewer.OnLeave.AddListener(RemoveViewer);
-
-            return viewer;
-        }
-
-        private void OnInvite(Viewer viewer)
-        {
-            viewersToAdd++;
-            StartCoroutine(Cooldown(viewer));
-        }
-
-        private IEnumerator Cooldown(Viewer viewer)
-        {
-            yield return new WaitForSeconds(InvitationCooldown);
-            viewer.canInvite = true;
-        }
-
-        private void AddViewer()
-        {
-            Viewers.Add(BuildViewer());
-            OnViewersCountChanged.Invoke(CurrentNumberOfViewers);
-        }
-
-        private void RemoveViewer(Viewer viewer)
-        {
-            viewer.OnInvite.RemoveAllListeners();
-            viewer.OnLeave.RemoveAllListeners();
-            Viewers.Remove(viewer);
-            OnViewersCountChanged.Invoke(CurrentNumberOfViewers);
-
-            if (CurrentNumberOfViewers <= 0)
-            {
-                StoryEvents.Instance.OnLostAllViewers.Invoke();
-            }
-        }
     }
 }
