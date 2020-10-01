2187d808e3e94803e2a5f1d87db2562ef8b62740
Radomir Wojtera <radomir.wojtera@gmail.com>
2020-10-02 00:16:14 +0200

    bug fix

--- Assets/FadeToBlack.cs
+++ Assets/FadeToBlack.cs
@@ -9,13 +9,15 @@ public class FadeToBlack : MonoBehaviour
     public Image Image;
     public void FadeOut()
     {
-        GetComponent<Canvas>().sortingOrder = 20;
+        if (GetComponent<Canvas>() != null)
+            GetComponent<Canvas>().sortingOrder = 20;
         Image.GetComponent<Animator>().SetBool("Visible", false);
     }
 
     public void FadeIn()
     {
-        GetComponent<Canvas>().sortingOrder = 0;
+        if (GetComponent<Canvas>() != null)
+            GetComponent<Canvas>().sortingOrder = 0;
         Image.GetComponent<Animator>().SetBool("Visible", true);
     }
 }
